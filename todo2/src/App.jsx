0c1da70b171bf2600303848133be912b59d57cdc
@greta2025 <carolinemarcoux.laf@gmail.com>
2026-03-06 14:12:07 +0100

    composant container

--- todo2/src/App.jsx
+++ todo2/src/App.jsx
@@ -1,13 +1,14 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 
-
+import { TaskContainer } from "./components/taskContainer"
 
 
 function App() {
 
   return (
-    <>  
+    <> 
+    <TaskContainer /> 
     </>
   )
 }
---
+++ todo2/src/components/taskContainer.jsx
@@ -0,0 +1,8 @@
+// composant pour afficher les tâches
+export const TaskContainer = () => {
+    return (
+        <main>
+        Contenu ici
+        </main>
+    )
+}
