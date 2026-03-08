c5ac0f1ec09c140266e94697b37b5d40f3d40eb4
@greta2025 <carolinemarcoux.laf@gmail.com>
2026-03-08 19:02:40 +0100

    debut-incrementation-tâches

--- todo2/src/components/taskContainer.jsx
+++ todo2/src/components/taskContainer.jsx
@@ -3,11 +3,26 @@ import { Header } from "./header/header";
 import { TaskInput } from "./taskInput/taskInput";
 import { TaskList } from "./taskList/taskList";
 import { Footer } from "./footer/footer";
+import { useState } from "react";
+
 export const TaskContainer = () => {
+    const [taskslist, setTaskslist] = useState([
+    
+    ]); 
+
+    const addTask = (title) => {
+        const newTask = {
+            id: taskslist.length + 1,
+            title: title,
+            completed: false
+        };
+        setTaskslist([...taskslist, newTask]);
+    };
+
     return (
         <main>
         <Header />
-        <TaskInput />
+        <TaskInput addTask={addTask} />
         <TaskList />
         <Footer />
         </main>
--- todo2/src/components/taskInput/taskInput.jsx
+++ todo2/src/components/taskInput/taskInput.jsx
@@ -1,14 +1,29 @@
 // champ de saisie
 import styles from './taskInput.module.css';
-export const TaskInput = () => {
+import { useState } from "react";
+
+export const TaskInput = ({ addTask }) => {
+  const [taskTitle, setTastTitle] = useState('');
+  const handleInputChange = (e) => {
+    setTastTitle(e.target.value);
+  }
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    addTask(taskTitle);
+    setTastTitle('');
+  }
+
+
     return (
       <div className={'box ${styles.element'}>
         <h2 className={styles.title}>Ecrire la prochaine tâche</h2>
-        <form className={styles.containesr}>
+        <form className={styles.containesr} onSubmit={handleAddTask}>
           <input 
             className={styles.input} 
             type="text" 
-            placeholder="Ajouter une tâche" />
+            placeholder="Ajouter une tâche" 
+            onChange={handleInputChange} 
+            /> 
           <button 
             className="button-primary"
             type="submit"> 
